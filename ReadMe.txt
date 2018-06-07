2e5073cebd19d835a3da6156fad06282307f59b6
Sarah Bartley <roseelizabeth.sarah@gmail.com>
2018-06-06 19:19:13 -0700

    Final version

--- ReadMe.txt
+++ ReadMe.txt
@@ -1,38 +1,34 @@
-Tido the Space Dog 
+Tido the Space Dog
 
 
 The Concept
 
-This children's mini game is designed to be a promotional piece for Netflix's new up and coming children's show/movie "Tide the Space Dog"! 
+This children's mini game is designed to be a promotional piece for Netflix's new up and coming children's show/movie "Tido the Space Dog"!
 
 
 The Code (main.js)
 
 Example of a hand-coded function:
 
-Line 29 to 59 (craterClick)
-Line 62 to 66 (Click the Play Button)
-Line 69 to 77 (Click the Play Again button and reset the game)
+Line 29 to 61 (craterClick)
 
 Example of an array:
 
 Line 19 (gridArray set up)
-Line 84 to 86 (gridArray populated with "false")
-Line 89 to 93 (grid spaces reserved for game instructions and random others for design purposes)
-Line 107 (newCrater placed in gridArray)
+Line 87 to 89 (creating second dimension of the array and populating with "false")
+Line 92 to 96 (grid spaces reserved for game instructions and random others for design purposes)
+Line 113 (newCrater placed in gridArray)
 
 Example of a loop:
 
-Line 84 to 86 
-Line 95 to 128
-
+Line 87 to 89
+Line 99 to 136
 
 Example of conditionals:
 
-Line 31 to 58 
+Line 31 to 60
 
 
-Example of HTML interaction: 
+Example of HTML interaction:
 
-
-
+Line 29 to 79 (onClick)
--- js/main.js
+++ js/main.js
@@ -25,7 +25,7 @@ $(document).ready(function(){
       sadTido = $("#sad-tido");
 
 
-  // handlers
+  // check for win when crater clicked
   function craterClick(){
     const crater = $(this);
     if (crater.data("craterIndex") === boneIndex){
@@ -35,12 +35,13 @@ $(document).ready(function(){
       // temporarily toggle sad tido
       sadTido.toggle();
       happyTido.toggle();
+      // toggle it back to happy tido
       setTimeout(function(){
         sadTido.toggle();
         happyTido.toggle();
       }, 1000);
 
-      //generate an x and place it on crater
+      // generate an x and place it on crater
       const craterPos = crater.position();
       const craterHeight = crater.height();
       const craterWidth = crater.width();
@@ -51,6 +52,7 @@ $(document).ready(function(){
       const errorHeight = newError.height();
       const errorWidth = newError.width();
 
+      // center the error on the crater
       newError.show().offset({
         top: craterPos.top + craterHeight/2 - errorHeight/2,
         left: craterPos.left + craterWidth/2 - errorWidth/2,
@@ -58,7 +60,7 @@ $(document).ready(function(){
     }
   }
 
-  // actions
+  // start game
   playButton.click(function(){
     splashPage.hide();
     gameboard.show();
@@ -81,6 +83,7 @@ $(document).ready(function(){
   instructions.hide();
   sadTido.hide();
 
+  // add second dimension to the array and populate with "false"
   for (i = 0; i < GRID_SIZE; i++) {
     gridArray[i] = new Array(GRID_SIZE).fill(false);
   }
@@ -92,16 +95,19 @@ $(document).ready(function(){
   gridArray[GRID_SIZE - 1][GRID_SIZE - 1] = true;
   gridArray[0][GRID_SIZE - 1] = true;
 
+  // get a random position
   for(i = 0; i < MAX_CRATERS; i++){
     let colPosition = Math.floor(getRandom(0, GRID_SIZE));
     let rowPosition = Math.floor(getRandom(0, GRID_SIZE));
 
+    // check if the position is occupied
+    // if occupied, generate a new position and check again
     while(gridArray[colPosition][rowPosition]){
       colPosition = Math.floor(getRandom(0, GRID_SIZE));
       rowPosition = Math.floor(getRandom(0, GRID_SIZE));
     }
 
-    // make a copy of the crater image and put into array
+    // make a copy of the crater image and put into array at generated position
     let newCrater = craterTemplate.clone();
 
     gridArray[colPosition][rowPosition] = newCrater;
@@ -112,11 +118,13 @@ $(document).ready(function(){
     // show the crater, set a random width and store the craterIndex
     newCrater.show().width(getRandom(70, 200)).data("craterIndex", i);
 
+
+    // set the position of the crater on the gameboard 
     const colWidth = gameboard.width() / GRID_SIZE;
-    const colHeight = gameboard.height() / GRID_SIZE;
+    const rowHeight = gameboard.height() / GRID_SIZE;
 
     const newLeft = colPosition * colWidth;
-    const newTop = rowPosition * colHeight;
+    const newTop = rowPosition * rowHeight;
 
     newCrater.offset({
       top: newTop,
